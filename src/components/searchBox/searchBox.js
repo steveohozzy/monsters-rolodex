85e4a874c9ab07ecc025c112dd39f2fa0499eb1e
Your Name <you@example.com>
2025-06-17 12:49:27 +0100

    amended search box to not be class based component

--- src/components/searchBox/searchBox.js
+++ src/components/searchBox/searchBox.js
@@ -1,18 +1,13 @@
-import { Component } from "react";
 import './searchBox.css';
 
-class SearchBox extends Component {
-    render() {
-        const {className, placeholder, onChangeHandler} = this.props;
-
-        return (
-            <input className={className} type='search' placeholder={placeholder}
-                onChange={
-                    onChangeHandler
-                }
-            />
-        )
-    }
+const SearchBox = ({ className, placeholder, onChangeHandler}) => {
+    return (
+        <input className={className} type='search' placeholder={placeholder}
+            onChange={
+                onChangeHandler
+            }
+        />
+    )
 }
 
 export default SearchBox;
